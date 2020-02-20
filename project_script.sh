2e71fe1e87d52bfb6f61f3d0fb86060c75a1ab56
shishirkhandelwalgithub <61186422+shishirkhandelwalgithub@users.noreply.github.com>
2020-02-20 17:26:19 +0530

    Update project_script.sh

--- project_script.sh
+++ project_script.sh
@@ -4,7 +4,7 @@ mkdir clone_folder
 cd clone_folder
 git init
 #git clone https://github.com/shishirkhandelwal29/project1.git
-git clone https://918f481b768dce46d89756998680493473906f17:x-oauth-basic@github.com/shishirkhandelwal29/project1.git
+#git clone https://918f481b768dce46d89756998680493473906f17:x-oauth-basic@github.com/shishirkhandelwal29/project1.git
 var1=$(find . -name 'commit-msg' -print)
 echo "enter replacement string"
 read var2
