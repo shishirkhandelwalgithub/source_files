bedda5fda78b91e9eec7aa99d56f17c07e8fd4da
shishir <shishir.khandelwal@sourcefuse.com>
2020-02-20 16:42:26 +0530

    Add code to do 'file and replace'

--- project_script.sh
+++ project_script.sh
@@ -4,6 +4,10 @@ mkdir clone_folder
 cd clone_folder
 git init
 git clone https://github.com/shishirkhandelwal29/project1.git
-ls
+var1=$(find . -name 'commit-msg' -print)
+echo "enter replacement string"
+read var2
+sed -i -e "s/DEVOPS/$var2/g" $var1
+cd project1
 echo "Script terminating..."
 
