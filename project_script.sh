79526e25d9031cea622c0ef11108cddbc87f905a
shishir <shishir.khandelwal@sourcefuse.com>
2020-02-20 16:38:16 +0530

    Create clone

--- project_script.sh
+++ project_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Script starting..."
+mkdir clone_folder
+cd clone_folder
+git init
+git clone https://github.com/shishirkhandelwal29/project1.git
+ls
 echo "Script terminating..."
 
