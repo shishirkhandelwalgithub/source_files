74aff2d2564bc5319ed4dee5460c52044a2398c5
shishir <shishir.khandelwal@sourcefuse.com>
2020-02-20 17:39:31 +0530

    latest working code

--- project_script.sh
+++ project_script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 echo "Script starting..."
-mkdir clone_folder
-cd clone_folder
+mkdir solution123
+cd solution123
 git init
-#git clone https://github.com/shishirkhandelwal29/project1.git
-#git clone https://918f481b768dce46d89756998680493473906f17:x-oauth-basic@github.com/shishirkhandelwal29/project1.git
+#token=$(cat auth_file.txt)
+echo "enter token"
+read token
+git clone https://$token:x-oauth-basic@github.com/shishirkhandelwal29/project1.git
 var1=$(find . -name 'commit-msg' -print)
-echo "enter replacement string"
+echo "Enter replacement string"
 read var2
+echo "DEVOPS would be replaced with $var2\n"
 sed -i -e "s/DEVOPS/$var2/g" $var1
 cd project1
 git add .
-git commit -m ' commit the changes into repo'
+git commit -m 'new_commit_hope1'
 git push -u origin master
 echo "Script terminating..."
+
+
 
