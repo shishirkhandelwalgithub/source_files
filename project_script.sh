ce803ca47f0ad51a650a3e07807934c93efed7e0
shishir <shishir.khandelwal@sourcefuse.com>
2020-02-25 17:11:00 +0530

    remove manual repo requirement

--- project_script.sh
+++ project_script.sh
@@ -2,9 +2,9 @@
 mkdir solution123
 cd solution123
 git init
-echo "enter link"
+echo "Link?"
 read link
-echo "enter token"
+echo "Token?"
 read token
 v1=${link:0:8}
 v2=${link:8:${#link}-8}
@@ -12,10 +12,14 @@ v3=:x-oauth-basic@
 newlink=$v1$token$v3$v2
 git clone $newlink
 var1=$(find . -name 'commit-msg' -print)
-echo "enter string"
+echo "New string?"
 read var2
 sed -i -e "s/DEVOPS/$var2/g" $var1
-cd project1
+repo=$(awk -F/ '{print $NF}' <<< $link)
+repo_name=$(awk -F. '{print $1}' <<< $repo)
+echo repo
+echo repo_name
+cd repo_name
 git add .
 git commit -m 'with only link'
 git push -u origin master
